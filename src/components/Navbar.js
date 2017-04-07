90f2539ddf423db283bf66205dc364168a309ac6
Cameron Baughn <cam.baughn@gmail.com>
2017-04-06 18:10:13 -0700

    Set up Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,23 @@
 import React, { Component } from 'react';
+
+
+const Navbar = () => {
+  return (
+    <div style={styles.background} >
+
+    </div>
+  )
+}
+
+
+const styles = {
+  background: {
+    width: '100%',
+    height: 60,
+
+    backgroundColor: '#F7BC5A',
+  },
+}
+
+
+export default Navbar;
